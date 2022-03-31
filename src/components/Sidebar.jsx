09638fe5c85bdd2e62659cb4e6814e41ef985d0c
BrunoSantosCosta <brunosanttos667@gmail.com>
2022-03-31 17:44:10 -0300

    Creation Navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,38 +11,10 @@ import { FiLogOut } from "react-icons/fi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { VscChromeClose } from "react-icons/vsc";
 import scrollreveal from "scrollreveal";
+
+
 export default function Sidebar() {
   const [currentLink, setCurrentLink] = useState(1);
-  const [navbarState, setNavbarState] = useState(false);
-  const html = document.querySelector("html");
-  html.addEventListener("click", () => setNavbarState(false));
-
-  useEffect(() => {
-    const sr = scrollreveal({
-      origin: "left",
-      distance: "80px",
-      duration: 1000,
-      reset: false,
-    });
-
-    sr.reveal(
-      `
-          .brand,
-          .links>ul>li:nth-of-type(1),
-      .links>ul>li:nth-of-type(2),
-      .links>ul>li:nth-of-type(3),
-      .links>ul>li:nth-of-type(4),
-      .links>ul>li:nth-of-type(5),
-      .links>ul>li:nth-of-type(6),
-      .logout
-      `,
-      {
-        opacity: 0,
-        interval: 300,
-      }
-    );
-  }, []);
-
   return (
     <>
       <Section>
@@ -51,18 +23,7 @@ export default function Sidebar() {
             <FaTaxi />
             <span>MY TRIPS</span>
           </div>
-          <div className="toggle">
-            {navbarState ? (
-              <VscChromeClose onClick={() => setNavbarState(false)} />
-            ) : (
-              <GiHamburgerMenu
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setNavbarState(true);
-                }}
-              />
-            )}
-          </div>
+          <div className="toggle"></div>
           <div className="links">
             <ul>
               <li
@@ -129,66 +90,6 @@ export default function Sidebar() {
           </a>
         </div>
       </Section>
-      <ResponsiveNav state={navbarState} className={navbarState ? "show" : ""}>
-        <div className="responsive__links">
-          <ul>
-            <li
-              className={currentLink === 1 ? "active" : "none"}
-              onClick={() => setCurrentLink(1)}
-            >
-              <a href="#">
-                <MdSpaceDashboard />
-                <span> Dashboard</span>
-              </a>
-            </li>
-            <li
-              className={currentLink === 2 ? "active" : "none"}
-              onClick={() => setCurrentLink(2)}
-            >
-              <a href="#">
-                <RiDashboard2Fill />
-                <span> Riders</span>
-              </a>
-            </li>
-            <li
-              className={currentLink === 3 ? "active" : "none"}
-              onClick={() => setCurrentLink(3)}
-            >
-              <a href="#">
-                <FaAddressCard />
-                <span> Payment Details</span>
-              </a>
-            </li>
-            <li
-              className={currentLink === 4 ? "active" : "none"}
-              onClick={() => setCurrentLink(4)}
-            >
-              <a href="#">
-                <GiTwirlCenter />
-                <span> Learning Center</span>
-              </a>
-            </li>
-            <li
-              className={currentLink === 5 ? "active" : "none"}
-              onClick={() => setCurrentLink(5)}
-            >
-              <a href="#">
-                <BsFillChatTextFill />
-                <span> FAQs</span>
-              </a>
-            </li>
-            <li
-              className={currentLink === 6 ? "active" : "none"}
-              onClick={() => setCurrentLink(6)}
-            >
-              <a href="#">
-                <IoSettings />
-                <span> Settings</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </ResponsiveNav>
     </>
   );
 }
@@ -303,52 +204,6 @@ const Section = styled.section`
     .top > .links,
     .logout {
       display: none;
-    }
-  }
-`;
-
-const ResponsiveNav = styled.div`
-  position: fixed;
-  right: -10vw;
-  top: 0;
-  z-index: 10;
-  background-color: #000;
-  height: 100vh;
-  width: ${({ state }) => (state ? "60%" : "0%")};
-  transition: 0.4s ease-in-out;
-  display: flex;
-  opacity: 0;
-  visibility: hidden;
-  padding: 1rem;
-  .responsive__links {
-    ul {
-      list-style-type: none;
-      display: flex;
-      flex-direction: column;
-      gap: 1rem;
-      margin-top: 3rem;
-      li {
-        padding: 0.6rem 1rem;
-        border-radius: 0.6rem;
-        &:hover {
-          background-color: #04BFAD;
-          a {
-            color: #000;
-          }
-        }
-        a {
-          text-decoration: none;
-          display: flex;
-          gap: 1rem;
-          color: #FFF;
-        }
-      }
-      .active {
-        background-color: #04BFAD;
-        a {
-          color: #000;
-        }
-      }
     }
   }
 `;
